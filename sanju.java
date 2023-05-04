b31dae31700287c6627b5bbe7824a9aae985bfe6
SanjuRawatdev <sanjurawatdevsoftdev@gmail.com>
2023-05-04 07:03:26 +0530

    New Changes

--- sanju.java
+++
@@ -1,9 +0,0 @@
-class Sanju
-{
-  public static void main(String arr[]){
-     System.out.println("hello world");
-	 int num1=10,num2=20,num3=0;
-	 num3=num1+num2;
-	 System.out.println(num3);
-  }
-}
