f47e7c27f2598077adfed0d4fda6c63fd4eda681
SanjuRawatdev <sanjurawatdevsoftdev@gmail.com>
2023-05-06 10:33:47 +0530

    hi

---
+++ Expticit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Expticit {
+   public static void main(String arr[])
+   {
+    byte a;
+    float f=5.3f;
+    a=(byte)f;
+    System.out.println(a);
+   } 
+}
---
+++ Impticit.java
@@ -0,0 +1,10 @@
+//impticit type casting
+import java.util.*;
+class Impticit {
+   public static void main(String arr[]) {
+    byte a=5;
+    float f;
+    f=a;
+    System.out.println(f);
+   } 
+}
